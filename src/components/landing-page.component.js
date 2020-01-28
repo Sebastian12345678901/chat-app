10f5d4a482480d9c509c6cf81a266440c4ba0d2d
MYIIIKETEPHomE <Denis.jakusjev@gmail.com>
2020-01-28 13:06:50 +0100

    lol

--- src/components/landing-page.component.js
+++ src/components/landing-page.component.js
@@ -9,13 +9,12 @@ import axios from "axios"
 export default class LandingPage extends Component {
     constructor(props) {
         super(props);
-
-
         this.state = {
-
+            stage : 0,
             userID: "",
             password: "",
-            email: ""
+            email: "",
+            statusMessage:""
         }
     }
 
@@ -26,13 +25,11 @@ export default class LandingPage extends Component {
         })
 
     }
-
     onChangePassword = (e) => {
         this.setState({
             password: e.target.value
         })
     }
-
     onChangeEmail = (e) => {
         this.setState({
             email: e.target.value
@@ -48,61 +45,97 @@ export default class LandingPage extends Component {
         }
 
         axios.post("http://localhost:5000/users/reg", user)
-            .then(res => console.log(res.data)).catch((error) => {
-                console.log(error)
+            .then(res => {console.log(res.data)
+                this.setState({
+                    statusMessage:"Success! you have been registered with username" + user.userID,
+                    stage:0,
+                    password:"",
+                    email:""
+                })}).catch((error) => {
+                console.log(error);
+                this.setState({
+                    statusMessage:"SOMETHING WENT WRLY WRONG"
+                })
             });
 
         console.log(user);
 
-        window.location.assign("https://localhost:3000/create");
 
     }
 
-
+    goToReg = () => {
+        this.setState({
+            stage:1
+        })
+    }
     render() {
         return (
             <div>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label >
-                            Username
-                        </label>
-
-                        <input
-                            required
-                            className="form-control"
-                            value={this.state.userID}
-                            onChange={this.onChangeUsername} />
-                    </div>
-
-                    <div className="form-group">
+            {this.state.stage == 0 &&
+            <div>
+                <label>Login</label><br></br>
+                {this.state.statusMessage}
+            <form onSubmit={this.login}>
+                
+                
+                <label>Username</label>
+                <input required className="form-control"
+                value = {this.state.userID}
+                onChange = {this.onChangeUsername}/>
+                 <div className="form-group">
                         <label >
                             Password
                         </label>
-
                         <input
                             required
                             className="form-control"
                             value={this.state.password}
                             onChange={this.onChangePassword} />
                     </div>
-
+            </form>
+            <button class = "btn btn-primary" onClick={this.goToReg}>REGME</button>
+            </div>
+            }
+             {this.state.stage == 1 && 
+             <div>
+                 {this.state.statusMessage}
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label >
+                            Username
+                        </label>
+                        <input
+                            required
+                            className="form-control"
+                            value={this.state.userID}
+                            onChange={this.onChangeUsername} />
+                    </div>
+                    <div className="form-group">
+                        <label >
+                            Password
+                        </label>
+                        <input
+                            required
+                            className="form-control"
+                            value={this.state.password}
+                            onChange={this.onChangePassword} />
+                    </div>
                     <div className="form-group">
                         <label >
                             Email
                         </label>
-
                         <input
                             required
                             className="form-control"
                             value={this.state.email}
                             onChange={this.onChangeEmail} />
                     </div>
-
                     <div className="form-group">
                         <input type="submit" value="Create new user" className="btn btn-primary" />
                     </div>
                 </form>
+                </div>
+                }
             </div>
 
         )
